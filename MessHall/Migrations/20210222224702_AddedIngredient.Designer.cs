eeb3e18feeb38b4963778c94a80b495b30c7f9f4
Devon McCauley <54080684+DevonMcCauley@users.noreply.github.com>
2021-02-22 17:53:02 -0500

    Generate migrations to add Ingredient and include link to Ingredient List

--- MessHall/Migrations/20210222224702_AddedIngredient.Designer.cs
+++ MessHall/Migrations/20210222224702_AddedIngredient.Designer.cs
@@ -3,14 +3,16 @@ using MessHall.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MessHall.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210222224702_AddedIngredient")]
+    partial class AddedIngredient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
