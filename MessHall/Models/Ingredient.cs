f3c814c45563c25200872d29e481b739f3e07b3e
Devon McCauley <54080684+DevonMcCauley@users.noreply.github.com>
2021-02-22 16:49:36 -0500

    Create Ingredient.cs

---
+++ MessHall/Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessHall.Models
+{
+    public class Ingredient
+    {
+
+        public int IngredientId { get; set; }
+        [Required, MaxLength(35)]
+        public string Name { get; set; }
+        [Required, MaxLength(100)]
+        public string Description { get; set; }
+        [Required]
+        public double CaloriesPerUnit { get; set; }
+
+    }
+}
